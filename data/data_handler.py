8c731c1f5bd6dc8dfdc0fb0b58b0bb7edefe5cb5
danishdata <danish_haroon@ymail.com>
2017-04-29 19:18:02 +0500

    pushing data app to remote

---
+++ data/data_handler.py
@@ -0,0 +1,10 @@
+import pymongo
+import pandas as pd
+
+class DataHandler(object):
+
+	@classmethod
+	def update_user_log_db(cls, fname = './dataset/large.csv', chunk_size = 1000):
+
+		for chunk in pd.read_csv(fname, chunksize = chunk_size,low_memory=True):
+			print chunk.shape()
